14eb5b45449312c740f929436c722ce5e33b1a1a
jitendrakatke <jitendra.katke@torinit.ca>
2022-06-13 21:49:26 +0530

    minor changes

--- app/Traits/sendMobileSMS.php
+++ app/Traits/sendMobileSMS.php
@@ -12,7 +12,7 @@ trait sendMobileSMS
         // Message details
         $numbers = array($mobile_number);
         $sender = urlencode($sender_id);
-        $message = urlencode($message);
+        // $message = urlencode($message);
 
         $numbers = implode(',', $numbers);
 
