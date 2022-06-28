2e851ea412e0b00651ed3244c45dbff63a80692c
jitendrakatke <jitendra.katke@torinit.ca>
2022-06-28 17:57:15 +0530

    minor changes

--- app/Http/Controllers/API/PatientController.php
+++ app/Http/Controllers/API/PatientController.php
@@ -1104,9 +1104,9 @@ class PatientController extends Controller
         if ($request->filled('sortBy')) {
             $sortBy = $validatedData['sortBy'];
         }
-        // if ($request->filled('orderBy')) {
-        //     $orderBy = $validatedData['orderBy'];
-        // }
+        if ($request->filled('orderBy')) {
+            $orderBy = $validatedData['orderBy'];
+        }
         $record = Appointments::where('patient_id', auth()->user()->id)->with('doctor:id,first_name,middle_name,last_name,profile_photo')->with('clinic_address')->with('prescription')->where(function ($query) use ($filter, $request) {
             
             if (array_key_exists('upcoming', $filter) && $filter['upcoming'] == 1) {
