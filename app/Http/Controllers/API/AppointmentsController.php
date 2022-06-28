0938310d62ad45373fa50d06e927b03a1b93ff61
jitendrakatke <jitendra.katke@torinit.ca>
2022-06-28 15:31:36 +0530

    date time format in sms send

--- app/Http/Controllers/API/AppointmentsController.php
+++ app/Http/Controllers/API/AppointmentsController.php
@@ -686,7 +686,7 @@ class AppointmentsController extends Controller
         // $message = "Appointment Confirmed Hai Dr". $record['doctor']['first_name']. ",New appointment confirmed for the following details. Patient Name:" .$record['current_patient_info']['user']['first_name'] ." ". $record['current_patient_info']['user']['last_name']. "Bookingid:". $record['appointment_unique_id'] ."type:". $record['consultation_type'] . "Date:" . $record['booking_date'] ." ". $record['time'] . "Place:" . $record['clinic_address']['clinic_name'] ." ". $record['clinic_address']['street_name'] ." ". $record['clinic_address']['city_village'] ." ". $record['clinic_address']['state'] . ".";
         $doctorname = $record['doctor']['first_name'];
         $patient_name = $record['current_patient_info']['user']['first_name'] . ' '. $record['current_patient_info']['user']['last_name'];
-        $date_time = date('j F', $record['booking_date']) . ' ' . date('h:m', $record['time']);
+        $date_time = date('j F', strtotime($record['booking_date'])) . ' ' . date('h:m', strtotime($record['time']));
         $appoitmentId = $record['appointment_unique_id'];
         $appointmentType = $record['consultation_type'];
         $clinic_name = $record['clinic_address']['clinic_name'];
